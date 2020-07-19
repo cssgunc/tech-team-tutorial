d6e4ea8eb5316756595e7c62307e864d80161bc1
lhzhang <lhzhang@live.unc.edu>
2020-07-18 21:43:28 -0400

    Game implementation is now accurate

--- react-projects/lucashzhang/src/index.js
+++ react-projects/lucashzhang/src/index.js
@@ -95,12 +95,14 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
         const squares = current.squares.slice();
+
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
@@ -138,8 +140,6 @@ function calculateWinner(squares) {
     }
     return null;
 }
-
-// ========================================
 
 ReactDOM.render(
     <Game />,
